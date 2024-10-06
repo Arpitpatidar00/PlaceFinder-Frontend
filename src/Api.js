c9cae88b2aae290d25470cbe5e06565b8541a8ca
Arpit Patidar <123389729+Arpitpatidar00@users.noreply.github.com>
2024-10-07 00:17:11 +0530

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,3 @@
 // Api.js
-const Api = process.env.REACT_APP_API_URL || "http://localhost:4000"; 
+const Api = process.env.REACT_APP_API_URL || "https://travelling-backend.onrender.com"; 
 export default Api;
